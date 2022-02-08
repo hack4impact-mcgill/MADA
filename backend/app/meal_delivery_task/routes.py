54bbaef5428cbe14c1cde6a4eed08350952bcde6
mahroo12 <mahroo.rahman@gmail.com>
2022-02-08 14:06:01 -0500

    added the endpoint for create task

--- backend/app/meal_delivery_task/routes.py
+++ backend/app/meal_delivery_task/routes.py
@@ -14,3 +14,35 @@ def delete_meal_delivery_task(id):
     db.session.commit()
 
     return jsonify(m.serialize)
+
+# create a mealDeliveryTask 
+@meal_delivery_task.route("", methods=["POST"])
+def create_meal_delivery_task():
+    data = request.get_json(force=True)
+    id = data.get("id")
+    address = data.get("address")
+    date = data.get("date")
+    time = data.get("time")
+    is_complete = data.get("is_complete")
+
+    # check if all fields are empty, if so it's a garbage post
+    if (
+        id == ""
+	    and address == ""
+        and date == ""
+        and time == ""
+        and (is_complete == False or is_complete == True)
+    ):
+        abort(400, "Cannot have all empty fields for a new task")
+
+    new_meal_delivery_task = MealDeliveryTask(
+	id = id,
+    	address = address,
+    	date = date,
+    	time = time,
+    	is_complete = is_complete,
+    )
+
+    db.session.add(new_meal_delivery_task)
+    db.session.commit()
+    return jsonify(new_meal_delivery_task.serialize)
